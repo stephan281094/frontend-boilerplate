e4be4841c21b33e40671dff9d7fb72042d3705d6
stephan281094 <stephan281094@gmail.com>
2016-07-14 21:40:24 +0200

    Add baseline toggler

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 require('./scss/index.scss')
+
+var wrapper = document.querySelector('.o-wrapper')
+var baselineToggler = document.querySelector('.c-baseline-toggler')
+
+baselineToggler.addEventListener('click', function () {
+  wrapper.classList.toggle('o-wrapper--show-baseline')
+})
