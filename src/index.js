41d3a9208545a556145294eb658641f4657f1788
stephan281094 <stephan281094@gmail.com>
2016-07-25 10:32:44 +0200

    Replace HTML and baseline CSS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
 require('./scss/index.scss')
-
-var wrapper = document.querySelector('.o-wrapper')
-var baselineToggler = document.querySelector('.c-baseline-toggler')
-
-baselineToggler.addEventListener('click', function () {
-  wrapper.classList.toggle('o-wrapper--show-baseline')
-})
